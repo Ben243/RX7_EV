c648db9bb186496ef51ddae14cffeebbc29ae015
Ben243 <49301702+Ben243@users.noreply.github.com>
2020-07-16 15:36:04 -0700

    added serial, can, and error messaging code

--- gpio.py
+++ gpio.py
@@ -1,16 +1,48 @@
 #!/usr/bin/env python
 '''
-Python Implementation of GPIO functionality for the RX7 Raspberry Pi 4 module
+Python 3 Implementation of GPIO and serial functionality 
+for the RX7 Raspberry Pi 4 module
 ''' 
-
 import RPi.GPIO as GPIO
-import subprocess
 from gpiozero import LED, Button
+import subprocess
+import os, sys
+import serial
+# import can
 from time import sleep
 
+'''
+SERIAL INITIALIZATION
+'''
+# inputs separated by spaces:
+# cell volts, 12 v battery voltage, cell temps, errmsg airtemp
+usb0 = serial.Serial('/dev/ttyUSB0', 9600) # usb serial input 1
+
+# usb1 = serial.Serial('/dev/ttyUSB1', 9600) 
+
+'''
+CAN INITIALIZATION (TO BE IMPLEMENTED) 
+'''
+# # stuff below is to initialize the can module to receive messages
+# os.system('sudo ip link set can0 type can bitrate 100000')
+# os.system('sudo ifconfig can0 up')
+
+# can0 = can.interface.Bus(channel = 'can0', bustype = 'socketcan_ctypes')# socketcan_native
+
+# #msg = can.Message(arbitration_id=0x123, data=[0, 1, 2, 3, 4, 5, 6, 7], extended_id=False)
+# msg = can0.recv(10.0)
+# print msg
+# if msg is None:
+#     print('Timeout occurred, no message.')
+
+# os.system('sudo ifconfig can0 down')
+
+'''
+GPIO INITIALIZATION
+'''
 # inputs
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Pi button
+GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Pi power button
 GPIO.setup(2, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Ignition 
 GPIO.setup(4 GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # ac switch
 GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # ac over pressure switch
@@ -38,10 +70,49 @@ def charge(channel) # turns on pumps and charger
     GPIO.output(21, 1) # charger on
     GPIO.output(26, 1) # charger on
 
+'''event detects'''
+
 #if falling voltage is detected on pin 3, shut down
 GPIO.add_event_detect(3, GPIO.FALLING, callback=shutdown_seq)
-#if falling voltage is detected on pin 4, charge
-GPIO.add_event_detect(4, GPIO.FALLING, callback=charge)
+# #if falling voltage is detected on pin 4, charge
+# GPIO.add_event_detect(4, GPIO.FALLING, callback=charge)
 
 
-# cell volts, 12 v battery voltage, cell temps, errmsg airtemp
+''' variable name declarations '''
+ign_switch = False
+pi_button = False
+ac_switch = False
+acOver
+
+
+'''error declarations'''
+# All errors are just booleans
+acOverPressureError = False
+battVoltError = False
+battTempError = False
+motorInverterTempError = False
+battTempDevError = False
+battVoltDevError = False
+
+# error calls
+def check_errors(): # prints any errors and returns whether any are true
+    err_string = ''
+    if acOverPressureError: err_string += 'acOverPressureError '
+    if battVoltError: err_string += 'battVoltError '
+    if battTempError: err_string += 'battTempError ' 
+    if motorInverterTempError: err_string += 'motorInverterTempError ' 
+    if battTempDevError: err_string += 'battTempDevError '
+    if battVoltDevError: err_string += 'battVoltDevError '
+    print(err_string)
+    return (acOverPressureError or battVoltError or battTempError 
+            or motorInverterTempError or battTempDevError or battVoltDevError)
+
+def update_errors():
+    return
+
+while True:
+    sleep(0.01)
+
+
+
+    
