3e097241cb1fc2e83fb0217f89773da3e8c83572
Ben243 <49301702+Ben243@users.noreply.github.com>
2020-08-28 23:50:50 -0700

    sdfsdfsf

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ from flask import Flask, Response, render_template
 app = Flask(__name__) # initialize flask
 
 '''global variables'''
-speed = 999
-rpm = 999
+speed = 99
+rpm = 99
 cell_volts = np.zeros(192) #[0 for x in range(192)] # 192, or 96*2 individual values
 batt_temps = np.zeros(16) #[0 for x in range(16)] # 16 battery temps
 # errmsg = False # arduino errors, see error declarations
@@ -60,70 +60,71 @@ def readSerial(ser):
     ln = ser.readline().split()
     return ln
 
+def generate_values():
+    #global variable declaration
+    # global ign_switch
+    # global charger_switch
+    # global ac_switch
+    # global ac_over_pressure_switch
+    # global dimmer_switch
+    global speed
+    global rpm
+    global cell_volts
+    global batt_temps
+    # #global errmsg
+    global air_temp
+    # global motor_inverter_temp
+    # global motor_stator_temp
+    # global high_pressure_refrig
+    # global lo_pressure_refrig
+    # global temp_refrig
+    # global ac_comp_pwm
+    # global chiller_pwm
+    # global cabin_pwm
+    # global fan_pwm
+    while True:
+        # usb0_data = readSerial(usb0) #from usb0
+        # input format is speed rpm ignition charging ac_on ac_pressure dimmer cellvolt_avg batttemp_avg 
+        # cell_volts = np.array(usb0_data[0:191]) # update cellvolts
+        # batt_temps = np.array(usb0_data[192:208]) # update batttemps
+        # air_temp = usb0_data[210] #check airtemp
+        
+        
+        # cell_mean = np.mean(cell_volts)
+        # batt_temp_mean = np.mean(batt_temps)
+        # max_cellv_dev =  max(np.abs(cell_mean - cell_volts))
+        # max_batt_temp_dev = max(np.abs(batt_temp_mean - batt_temps))
+        
+        # dummy values section
+        speed = (speed + 1) % 100
+        rpm = (rpm+0.25) % 8
+
+        json_data = json.dumps(
+            {'speed': speed,
+            'rpm': rpm,
+            # 'ignition': ign_switch, 
+            # 'charging': charger_switch, 
+            # 'twelvev': GPIO.input(20),
+            # 'avgcellvolts': cell_mean, 
+            # 'avgbatttemps': batt_temp_mean, 
+            # 'cellvoltdevmax': max_cellv_dev,
+            # 'batttempdevmax': max_batt_temp_dev,
+            'airtemp': air_temp,
+            # 'accomp': 99, 
+            # 'pump': 99, 
+            # 'allerrors': 1,
+            })
+        yield f"data:{json_data}\n\n"
+        # print(speed)
+        sleep(0.1) # update speed 
+
 #flask stuff
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/data')
-def data():
-    def generate_values():
-        #global variable declaration
-        # global ign_switch
-        # global charger_switch
-        # global ac_switch
-        # global ac_over_pressure_switch
-        # global dimmer_switch
-        global speed
-        global rpm
-        global cell_volts
-        global batt_temps
-        # #global errmsg
-        global air_temp
-        # global motor_inverter_temp
-        # global motor_stator_temp
-        # global high_pressure_refrig
-        # global lo_pressure_refrig
-        # global temp_refrig
-        # global ac_comp_pwm
-        # global chiller_pwm
-        # global cabin_pwm
-        # global fan_pwm
-        while True:
-            usb0_data = readSerial(usb0) #from usb0
-            # input format is speed rpm ignition charging ac_on ac_pressure dimmer cellvolt_avg batttemp_avg 
-            # cell_volts = np.array(usb0_data[0:191]) # update cellvolts
-            # batt_temps = np.array(usb0_data[192:208]) # update batttemps
-            # air_temp = usb0_data[210] #check airtemp
-            
-            
-            # cell_mean = np.mean(cell_volts)
-            # batt_temp_mean = np.mean(batt_temps)
-            # max_cellv_dev =  max(np.abs(cell_mean - cell_volts))
-            # max_batt_temp_dev = max(np.abs(batt_temp_mean - batt_temps))
-            
-            # dummy values section
-            speed = (speed + 1) % 100
-            rpm = (rpm+0.25) % 8
-
-            json_data = json.dumps(
-                {'speed': speed,
-                'rpm': rpm,
-                # 'ignition': ign_switch, 
-                # 'charging': charger_switch, 
-                # 'twelvev': GPIO.input(20),
-                # 'avgcellvolts': cell_mean, 
-                # 'avgbatttemps': batt_temp_mean, 
-                # 'cellvoltdevmax': max_cellv_dev,
-                # 'batttempdevmax': max_batt_temp_dev,
-                'airtemp': air_temp,
-                # 'accomp': 99, 
-                # 'pump': 99, 
-                # 'allerrors': 1,
-                })
-            yield f"data:{json_data}\n\n"
-            # print(speed)
-            sleep(0.5) # update speed   
+def data():  
     return Response(generate_values(), mimetype='text/event-stream')
 
 if __name__ == '__main__':
